d8311f82a6e59270c9ae6e1cf7ad05cc51c85649
Abimannan192 <dabimannan192@gmail.com>
2024-10-15 18:55:00 +0530

    code

--- FRONTEND/src/app/add-movie/add-movie.component.spec.ts
+++ FRONTEND/src/app/add-movie/add-movie.component.spec.ts
@@ -1,19 +1,93 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AddMovieComponent } from './add-movie.component';
 import { UserService } from '../user.service';
+import { MovieService } from '../movie.service';
+import { NgToastService } from 'ng-angular-popup';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of, throwError } from 'rxjs';
+import Swal from 'sweetalert2';
 
-describe('UserService', () => {
-  let service: UserService;
+class MockUserService {
+  getCurrentUser() {
+    return of({ lastName: 'Doe' });
+  }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      providers: [UserService]
-    });
-    service = TestBed.inject(UserService);
+  getLoginType() {
+    return of('admin');
+  }
+}
+
+class MockMovieService {
+  getMovies() {
+    return of({ getMovies: { movies: [{ movieName: 'Test Movie' }] } });
+  }
+
+  createMovie(data: any) {
+    return of({ message: 'Movie added Succesfully' });
+  }
+}
+
+class MockNgToastService {}
+
+describe('AddMovieComponent', () => {
+  let component: AddMovieComponent;
+  let fixture: ComponentFixture<AddMovieComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [AddMovieComponent],
+      providers: [
+        { provide: UserService, useClass: MockUserService },
+        { provide: MovieService, useClass: MockMovieService },
+        { provide: NgToastService, useClass: MockNgToastService },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddMovieComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should be added movie successfully', () => {
-    expect(service).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should initialize user and movies on ngOnInit', () => {
+    component.ngOnInit();
+
+    expect(component.user).toBe('Doe');
+    expect(component.login).toBe('admin');
+    expect(component.movies).toEqual([{ movieName: 'Test Movie' }]);
+    expect(component.name).toBe('Test Movie');
+  });
+
+  it('should return the form control for image', () => {
+    expect(component.image).toBeTruthy();
+  });
+
+  it('should submit the form and handle success', () => {
+    spyOn(component.movieService, 'createMovie').and.returnValue(of({ message: 'Movie added Succesfully' }));
+    spyOn(Swal, 'fire');
+
+    component.submit();
+
+    expect(component.movieService.createMovie).toHaveBeenCalled();
+    
+  });
+
+  it('should handle errors on submit', () => {
+    spyOn(component.movieService, 'createMovie').and.returnValue(throwError({ error: 'Error' }));
+
+    component.submit();
+
+    expect(component.displayUserStatus).toBeUndefined();
+  });
+
+  it('should reset the form', () => {
+    component.onReset();
+
+   
+    expect(component.movie.pristine).toBeTrue();
+    expect(component.movie.untouched).toBeTrue();
   });
 });
